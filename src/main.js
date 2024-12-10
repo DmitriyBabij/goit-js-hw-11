49fd3a9b50587d83f717e633c0b248cbcfaeb888
DmitriyBabij <babijdima@gmail.com>
2024-12-10 19:44:15 +0200

    hw11

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { fetchImages } from './js/pixabay-api.js';
-import { renderGallery, showNoResultsMessage, showLoadingIndicator, hideLoadingIndicator } from './render-functions.js';
+import { renderGallery, showNoResultsMessage, showLoadingIndicator, hideLoadingIndicator } from './js/render-functions.js';
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.search-form');
   const searchInput = document.querySelector('.search-input');
