a398e55e4da4d785a0d81075221a194dd787d084
DmitriyBabij <babijdima@gmail.com>
2024-12-12 21:06:49 +0200

    hw11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,15 +5,15 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export function renderGallery(images) {
   const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = ''; 
+  gallery.innerHTML = ''; // Очищаємо галерею перед додаванням нових зображень
 
   if (images.length === 0) {
-    
+    // Якщо зображень немає, показуємо повідомлення про відсутність результатів
     showNoResultsMessage();
     return;
   }
 
-
+  // Генерація та додавання нових зображень в галерею
   const markup = images
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `
@@ -32,15 +32,25 @@ export function renderGallery(images) {
   
   gallery.insertAdjacentHTML('beforeend', markup);
   
-  lightbox.refresh(); 
+  lightbox.refresh(); // Оновлюємо лайтбокс після завантаження нових зображень
 }
 
 export function showNoResultsMessage() {
   const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = '';
+  gallery.innerHTML = ''; // Очищаємо галерею перед відображенням повідомлення
 
   iziToast.error({
     title: 'Sorry',
     message: 'There are no images matching your search query. Please try again!',
   });
+}
+
+export function showLoadingIndicator() {
+  const loader = document.querySelector('.loader');
+  loader.classList.remove('hidden');
+}
+
+export function hideLoadingIndicator() {
+  const loader = document.querySelector('.loader');
+  loader.classList.add('hidden');
 }
