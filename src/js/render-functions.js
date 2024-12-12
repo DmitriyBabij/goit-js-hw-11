40cf8b2a4ed7f50ae35f49b5da416866526bdefd
DmitriyBabij <babijdima@gmail.com>
2024-12-12 21:00:32 +0200

    hw11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,11 +5,15 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export function renderGallery(images) {
   const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = '';
+  gallery.innerHTML = ''; 
+
   if (images.length === 0) {
-    showNoResultsMessage(); // Відображаємо повідомлення, якщо не знайдено зображень
+    
+    showNoResultsMessage();
     return;
   }
+
+
   const markup = images
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `
@@ -27,24 +31,16 @@ export function renderGallery(images) {
     .join('');
   
   gallery.insertAdjacentHTML('beforeend', markup);
-  const lightbox = new SimpleLightbox('.gallery a');
   
-  lightbox.refresh();
+  lightbox.refresh(); 
 }
 
 export function showNoResultsMessage() {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
+
   iziToast.error({
     title: 'Sorry',
     message: 'There are no images matching your search query. Please try again!',
   });
-}
-
-export function showLoadingIndicator() {
-  const loader = document.querySelector('.loader');
-  loader.classList.remove('hidden');
-}
-
-export function hideLoadingIndicator() {
-  const loader = document.querySelector('.loader');
-  loader.classList.add('hidden');
 }
