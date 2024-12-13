1347fe146d0911246d70b642fd3938d886b27be4
DmitriyBabij <babijdima@gmail.com>
2024-12-13 13:36:53 +0200

    hw11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -38,3 +38,20 @@ export function renderGallery(images) {
   // Оновлюємо lightbox після оновлення галереї
   lightbox.refresh();
 }
+
+export function showNoResultsMessage() {
+  iziToast.error({
+    title: 'Sorry',
+    message: 'There are no images matching your search query. Please try again!',
+  });
+}
+
+export function showLoadingIndicator() {
+  const loader = document.querySelector('.loader');
+  loader.classList.remove('hidden');
+}
+
+export function hideLoadingIndicator() {
+  const loader = document.querySelector('.loader');
+  loader.classList.add('hidden');
+}
